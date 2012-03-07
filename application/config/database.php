35334db095f0b2911e20b1a71b53d1dc82c12fbd
Eduardo <edu_merckx@yahoo.com.br>
2012-03-06 23:29:02 -0300

    cidoctrine

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '179325';
-$db['default']['database'] = 'teste';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
